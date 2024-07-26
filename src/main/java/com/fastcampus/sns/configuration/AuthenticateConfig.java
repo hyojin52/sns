d98b30e41208219cd75b810ed502b2b4d7c7aa44
hyojin <dongurami.kim@gmail.com>
2024-07-26 23:46:50 +0900

    feat: authentication entrypoint 설정

--- src/main/java/com/fastcampus/sns/configuration/AuthenticateConfig.java
+++ src/main/java/com/fastcampus/sns/configuration/AuthenticateConfig.java
@@ -1,5 +1,10 @@
 package com.fastcampus.sns.configuration;
 
+import com.fastcampus.sns.configuration.filter.JwtTokenFilter;
+import com.fastcampus.sns.exception.CustomAuthenticationEntryPoint;
+import com.fastcampus.sns.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,10 +12,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class AuthenticateConfig extends WebSecurityConfigurerAdapter {
+  
+  private final UserService userService;
+  
+  @Value("${jwt.secret-key}")
+  private String key;
   
   
   @Override
@@ -22,10 +34,10 @@ public class AuthenticateConfig extends WebSecurityConfigurerAdapter {
             .and()
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-    ;
-            // .and()
-            // .exceptionHandling()
-            // .authenticationEntryPoint()
+            .and()
+            .addFilterBefore(new JwtTokenFilter(key, userService), UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling()
+            .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
             
   }
 }
--- src/main/java/com/fastcampus/sns/controller/response/Response.java
+++ src/main/java/com/fastcampus/sns/controller/response/Response.java
@@ -13,7 +13,25 @@ public class Response<T> {
     return new Response<>(errorCode, null);
   }
   
+  public static Response<Void> success() {
+    return new Response<>("SUCCESS", null);
+  }
+  
   public static <T> Response<T> success(T result) {
     return new Response<>("SUCCESS", result);
+  }
+  
+  public String toStream() {
+    if(result == null) {
+      return "{ \n" +
+              "  \"resultCode\":" + "\"" + resultCode + "\", \n" +
+              "  \"result\":" + null +
+              "\n }";
+    }
+    
+    return "{ \n" +
+            "  \"resultCode\":" + "\"" + resultCode + "\", \n" +
+            "  \"result\":" + "\"" + result + "\"" +
+            "\n }";
   }
 }
