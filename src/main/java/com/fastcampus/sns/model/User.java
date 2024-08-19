39109a4089a7951257977b27c563a155b961691f
hyojin <dongurami.kim@gmail.com>
2024-08-19 16:49:23 +0900

    fix: 토큰 인증 시, user 에 redis 추가

--- src/main/java/com/fastcampus/sns/model/User.java
+++ src/main/java/com/fastcampus/sns/model/User.java
@@ -1,7 +1,10 @@
 package com.fastcampus.sns.model;
 
 import com.fastcampus.sns.model.entity.UserEntity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
@@ -13,13 +16,13 @@ import java.util.Collection;
 import java.util.List;
 
 @AllArgsConstructor
-@Getter
+@Data
 @NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class User implements UserDetails {
   
   private Integer id;
-  
-  private String userName;
+  private String username;
   private String password;
   private UserRole userRole;
   private Timestamp registeredAt;
@@ -39,31 +42,31 @@ public class User implements UserDetails {
   }
   
   @Override
+  @JsonIgnore
   public Collection<? extends GrantedAuthority> getAuthorities() {
     return List.of(new SimpleGrantedAuthority(this.getUserRole().toString()));
   }
   
   @Override
-  public String getUsername() {
-    return this.userName;
-  }
-  
-  @Override
+  @JsonIgnore
   public boolean isAccountNonExpired() {
     return this.deletedAt == null;
   }
   
   @Override
+  @JsonIgnore
   public boolean isAccountNonLocked() {
     return this.deletedAt == null;
   }
   
   @Override
+  @JsonIgnore
   public boolean isCredentialsNonExpired() {
     return this.deletedAt == null;
   }
   
   @Override
+  @JsonIgnore
   public boolean isEnabled() {
     return this.deletedAt == null;
   }
--- src/main/java/com/fastcampus/sns/service/UserService.java
+++ src/main/java/com/fastcampus/sns/service/UserService.java
@@ -6,6 +6,7 @@ import com.fastcampus.sns.model.Alarm;
 import com.fastcampus.sns.model.User;
 import com.fastcampus.sns.model.entity.UserEntity;
 import com.fastcampus.sns.repository.AlarmEntityRepository;
+import com.fastcampus.sns.repository.UserCacheRepository;
 import com.fastcampus.sns.repository.UserEntityRepository;
 import com.fastcampus.sns.util.JwtTokenUtils;
 import lombok.RequiredArgsConstructor;
@@ -23,6 +24,7 @@ public class UserService {
   private final UserEntityRepository userEntityRepository;
   private final BCryptPasswordEncoder encoder;
   private final AlarmEntityRepository alarmEntityRepository;
+  private final UserCacheRepository userCacheRepository;
   
   @Value("${jwt.secret-key}")
   private String secretKey;
@@ -31,9 +33,11 @@ public class UserService {
   private Long expiredTimeMs;
   
   public User loadUserByUserName(String userName) {
-    return userEntityRepository.findByUserName(userName).map(User::fromEntity).orElseThrow(() ->
-            new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not found", userName))
-    );
+    return userCacheRepository.getUser(userName).orElseGet(() -> {
+      return userEntityRepository.findByUserName(userName).map(User::fromEntity).orElseThrow(() ->
+              new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not found", userName))
+      );
+    });
   }
   
   @Transactional
@@ -50,13 +54,11 @@ public class UserService {
   
   public String login(String username, String password) {
     // 회원가입 여부 체크
-    UserEntity userEntity = userEntityRepository.findByUserName(username)
-            .orElseThrow(() ->
-                    new SnsApplicationException(ErrorCode.USER_NOT_FOUND, String.format("%s not founded", username))
-            );
+    User user = loadUserByUserName(username);
+    userCacheRepository.setUser(user);
     
     // 비밀번호 체크
-    if(!encoder.matches(password, userEntity.getPassword())) {
+    if(!encoder.matches(password, user.getPassword())) {
       throw new SnsApplicationException(ErrorCode.INVALID_PASSWORD, "");
     }
     
